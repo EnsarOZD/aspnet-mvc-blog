fe29ba476b1fd49f977c1105e7b3bd17d202fff5
Ensar ÖZDEMİR <ensarozd@gmail.com>
2023-05-30 11:27:05 +0300

    Controllers and Views added!

---
+++ App.Web.Mvc/AspNetMvcBlog/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvcBlog.Controllers
+{
+	public class AuthController : Controller
+	{
+		public IActionResult Register()
+		{
+			return View();
+		}
+		public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult ForotPassword()
+		{
+			return View();
+		}
+	}
+}
