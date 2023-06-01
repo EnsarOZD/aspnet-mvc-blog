828273a5216fc8e9203f2c750092a2c7d315f940
Ensar ÖZDEMİR <ensarozd@gmail.com>
2023-06-01 23:10:37 +0300

    Admin tamamlandı

--- App.Web.Mvc/AspNetMvcBlog/Controllers/Admin/AdminBaseController.cs
+++ App.Web.Mvc/AspNetMvcBlog/Controllers/Admin/AdminBaseController.cs
@@ -4,6 +4,7 @@ namespace AspNetMvcBlog.Controllers.Admin
 {
     public abstract class AdminBaseController : Controller
     {
+        [Route("admin/base/index")]
         public IActionResult Index()
         {
             return View();
